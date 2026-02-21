330fb750e0bdd853807e5bf829ad65fa1ac61d51
MomenAlbawaridGit <momen.albawarid@gmail.com>
2026-02-21 17:34:40 +0300

    fix the main

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 import mediapipe as mp
 from ultralytics import YOLO
 import time
+import os
+import subprocess
+import sys
 
 class FocusTracker:
     def __init__(self, penalty_video_path):
